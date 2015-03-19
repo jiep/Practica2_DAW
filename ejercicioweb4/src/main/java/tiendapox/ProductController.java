1b5bbf741419679ddc094138e86dbc3a55144439
jiep <jiep12345@hotmail.com>
2015-03-19 14:05:53 +0100

    Hechos algunos cambios

--- ejercicioweb4/src/main/java/tiendapox/ProductController.java
+++ ejercicioweb4/src/main/java/tiendapox/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
 	@RequestMapping("/")
 	public ModelAndView index() {
 		
-		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "0.png", "Descripción de prueba", 100));
+		products.add(new Product("SAMSUNG Galaxy S5 - negro - Smartphone", Category.PEQUEÑOS_ELECTRODOMESTICOS, "0.png", "El Galaxy S5 es una auténtica proeza técnica y es un smartphone Samsung de última generación equipado con las mejores tecnologías de telefonía móvil. Este teléfono portátil ultra conectado te permite disfrutar de la alta velocidad con la 4G y el Wifi-ac MIMO. Funciona con Android KitKat 4.4.2 y con un potente procesador Quad Core 2,5 GHz y dispone de 2 Gb de memoria RAM. Su línea con una fineza increíble pone de realce una magnífica pantalla Full HD de 5,1. Esta pantalla táctil  ultra luminosa con una fluidez alucinante garantiza una calidad de imagen excepcional gracias a la tecnología Super AMOLED. Gracias a ésta las imágenes se subliman y tus ojos disfrutarán a lo grande con nada más y nada menos que de 16 millones de colores.Una cámara de fotos  de 16 megapíxeles en la parte trasera del teléfono capta incluso las escenas más efímeras gracias a su enfoque automático de 0,3 segundos. Cuenta también con diversos modos fotográficos que mejoran tus tomas. La captura de vídeo es de calidad Ultra HD 2160p y no tiene nada que envidiarle a las mejores videocámaras digitales. Incluye también una cámara frontal de 2 megapíxeles para tus videoconferencias y videochats en particular.", 435));
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "1.png", "Descripción de prueba", 100));
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "2.png", "Descripción de prueba", 100));
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "0.png", "Descripción de prueba", 100));
@@ -35,8 +35,6 @@ public class ProductController {
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "0.png", "Descripción de prueba", 100));
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "1.png", "Descripción de prueba", 100));
 		products.add(new Product("Pepe", Category.PEQUEÑOS_ELECTRODOMESTICOS, "2.png", "Descripción de prueba", 100));
-
-
 		
 		return new ModelAndView("index").addObject("products", products);
 	}
@@ -45,27 +43,28 @@ public class ProductController {
 	public ModelAndView cart() {
 		return new ModelAndView("cart").addObject("products", products);
 	}
-	
+
 	@RequestMapping("/order")
-	public ModelAndView order(){
+	public ModelAndView order() {
 		return new ModelAndView("order").addObject("products", products);
 	}
-	
+
 	@RequestMapping("/admin")
-	public ModelAndView admin(){
+	public ModelAndView admin() {
 		return new ModelAndView("admin").addObject("products", products);
 	}
-	
+
 	@RequestMapping("/add")
-	public ModelAndView add(){
+	public ModelAndView add() {
 		return new ModelAndView("add").addObject("products", products);
 	}
 
-	/*@RequestMapping("/mostrar")
-	public ModelAndView mostrar(@RequestParam int numAnuncio) {
-
-		Anuncio anuncio = anuncios.get(numAnuncio-1);
-
-		return new ModelAndView("mostrar").addObject("anuncio", anuncio);
-	}*/
+	/*
+	 * @RequestMapping("/mostrar") public ModelAndView mostrar(@RequestParam int
+	 * numAnuncio) {
+	 * 
+	 * Anuncio anuncio = anuncios.get(numAnuncio-1);
+	 * 
+	 * return new ModelAndView("mostrar").addObject("anuncio", anuncio); }
+	 */
 }
